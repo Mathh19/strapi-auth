063f204af8690324b97247a62a5b40101a973dc8
Matheus <mf635097@gmail.com>
2022-10-13 08:27:26 -0300

    relacionado conteudo criado ao usuario logado

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -2,8 +2,17 @@
 
 /**
  * post controller
- */
+*/
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', {
+  async create(ctx) {
+    ctx.query = { ...ctx.query, user: ctx.state.user.id };
+      const { id } = ctx.state.user; //ctx.state.user contains the current authenticated user
+      const response = await super.create(ctx);
+      const updatedResponse = await strapi.entityService
+      .update('api::post.post', response.data.id, { data: { user: id } })
+    return updatedResponse;
+  },
+});
