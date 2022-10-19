cfedc7e7ad6abfe84f9f459ce903120729c48192
Matheus <mf635097@gmail.com>
2022-10-19 08:32:03 -0300

    Ajustando logica do metodo create, feito logica para find, findOne e count

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -8,24 +8,86 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::post.post', {
   async create(ctx) {
-    ctx.query = { ...ctx.query, user: ctx.state.user.id };
-    const { id } = ctx.state.user; //ctx.state.user contains the current authenticated user
+    const user = ctx.state.user;
     const response = await super.create(ctx);
-    const updatedResponse = await strapi.entityService
-      .update('api::post.post', response.data.id, { data: { user: id } });
+    const updatedResponse = await strapi.entityService.update('api::post.post', response.data.id, {
+      data: {
+        user: user.id,
+      }
+    });
+
     return updatedResponse;
   },
 
   async find(ctx) {
+    let response;
     const { id } = ctx.state.user;
-    const response = await super.find(ctx);
-    const updatedResponse = await strapi.entityService
-      .findMany('api::post.post', {
-        filters: { user: { id: id } },
-        populate: {
-          user: true,
+    const query = ctx.request.query;
+
+    if (query._q) {
+      response = await strapi.entityService
+        .findMany('api::post.post', {
+          filters: {
+            title: { $startsWith: query._q },
+            user: { id: id }
+          },
+          populate: {
+            user: true,
+          }
+        });
+    } else {
+      response = await strapi.entityService
+        .findMany('api::post.post', {
+          filters: { user: { id: id } },
+          populate: {
+            user: true,
+          }
+        });
+    }
+
+    return response;
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const userId = ctx.state.user.id;
+
+    const response = await strapi.db.query('api::post.post').findOne({
+      where: {
+        $and: [
+          {
+            id: id
+          },
+          {
+            user: { id: userId }
+          },
+        ],
+      },
+      populate: { user: true },
+    });
+    return response;
+  },
+
+  async count(ctx) {
+    let response;
+    const { id } = ctx.state.user;
+    const query = ctx.request.query;
+
+    if (query._q) {
+      response = await strapi.entityService.count('api::post.post', {
+        filters: {
+          title: { $startsWith: query._q },
+          user: { id: id }
         }
       });
-    return updatedResponse;
+    } else {
+      response = await strapi.entityService.count('api::post.post', {
+        filters: {
+          user: { id: id }
+        }
+      });
+    }
+
+    return response;
   }
 });
